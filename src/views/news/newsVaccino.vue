59cf01f0eb6dec966adc61b79e38933298a0c72d
alessia <alessia.cerami@studio.unibo.it>
2020-11-05 22:12:46 +0100

    added new images

---
+++ src/views/news/newsVaccino.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div style="padding: 30px 0">
+      <img src="../../assets/vaccino-antinfluenza.jpg"/>
+      <p class="vaccinoText"> Vista la grande adesione alla campagna sulla vaccinazione antinfluenzale da parte dei donatori di sangue, 
+        l’Azienda USL offre una nuova opportunità per aderire.<br/><br/>
+        I donatori di sangue infatti possono richiedere l’appuntamento per la vaccinazione, da eseguire presso gli ambulatori di Igiene Pubblica.
+        <br/> Occorre prenotarsi al CUP per richiedere la prenotazione per effettuare la vaccinazione. 
+        </p>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.container
+  .vaccinoText
+    font-family: 'Montserrat'
+    font-size: 1.3em
+    font-weight: 500
+    text-align: justify
+    padding-top: 30px 
+</style>
