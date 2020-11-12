2e9992206f3839719cfbd94df1f5e2b1c3497a0f
alessia <alessia.cerami@studio.unibo.it>
2020-11-12 17:44:52 +0100

    added owner

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@
       <navbar class="nav-bar"></navbar>
     </div>
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header/Header.vue';
+import Footer from './components/Footer/Footer.vue';
 import Navbar from './components/Navbar/Navbar.vue';
 export default {
   name: 'app',
   components: {
     Header,
     navbar: Navbar,
+    Footer,
   },
 }
 </script>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="footer">
+    <span class="owner">Owner and creator: <a class="linkSite" href="https://narcale.github.io">  Narcale</a></span>
+  </div>
+</template>
+
+<style lang="sass">
+@import 'Footer'
+</style>
