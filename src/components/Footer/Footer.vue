f838e8b21f69ed89c2abc85e6c0ba92f3122a873
alessia <alessia.cerami@studio.unibo.it>
2020-12-21 18:32:47 +0100

    facebook

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="footer">
+    <div class="social-links"> Seguici anche su:
+      <a href="https://www.facebook.com/Avis-Comunale-Forlimpopoli-114176303825612" class="facebook"> 
+        <i class="bx bxl-facebook" ></i>
+      </a>
+    </div>
+    <hr/>
     <span class="owner">Owner and creator: <a class="linkSite" href="https://narcale.github.io">  Narcale</a></span>
   </div>
 </template>
