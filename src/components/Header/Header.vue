1ee85d5e1d5b6ec2f1ba264fa8ee707424935bd8
alessia <alessia.cerami@studio.unibo.it>
2020-12-23 21:52:49 +0100

    alt into navbar

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,7 +2,7 @@
   <div class="grid-container">
     <div class="grid-container-text">
       <div class="grid-item">
-        <img class="img-logo" src="../../assets/logo_avis.png">
+        <img class="img-logo" alt="img_logo" src="../../assets/logo_avis.png">
       </div>
       <div class="grid-item text">
         <span>Se molti donano sangue, il sangue c'è per tutti.</span>
