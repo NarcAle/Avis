1fb3a1fda46a4cee20e108ac51a917093b1b336d
alessia <alessia.cerami@studio.unibo.it>
2021-05-08 22:12:24 +0200

    added images in gallery

--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container">
     <div class="bannerVaccino">
-      <div><p class="textBanner">Notizie in evidenza</p></div>
+      <div><p class="textBanner">Media Avis Forlimpopoli</p></div>
       <hr/>
-      <img src="../../assets/vaccinoR.jpg" alt="vaccino" @click="redirectVaccino" class="hoverImg" style="width: 50%: "/>    
+      <div class="fiori">
+        <img src="../../assets/vaccinoR.jpg" alt="vaccino" @click="redirectVaccino" class="hoverImg"/>  
+        <img src="../../assets/fiorimpopoli1.jpeg" alt="vaccino" @click="redirectVaccino" class="hoverImg"/>  
+        <img src="../../assets/fiorimpopoli2.jpeg" alt="vaccino" @click="redirectVaccino" class="hoverImg"/>  
+        <img src="../../assets/fiorimpopoli3.jpeg" alt="vaccino" @click="redirectVaccino" class="hoverImg"/>  
+        <img src="../../assets/fiorimpopoli4.jpeg" alt="vaccino" @click="redirectVaccino" class="hoverImg"/> 
+      </div>
     </div>
     <hr/>
 
@@ -24,11 +30,14 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.fiori
+  display: flex
+  flex-wrap: wrap
+ 
 .hoverImg
   width: 100%
   border: 1px solid black
-  display: flex
-  flex-wrap: wrap
+
 @media screen and (min-width: 992px)
   .hoverImg
     width: 50%
